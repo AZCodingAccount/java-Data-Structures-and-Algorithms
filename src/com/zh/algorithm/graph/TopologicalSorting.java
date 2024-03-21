5f912adfa94a81c007bd01ffcdf6d2093bfd2e3c
AlbertZhang <han892577@163.com>
2024-03-21 21:55:27 +0800

    拓扑排序

--- src/com/zh/algorithm/graph/TopologicalSorting.java
+++ src/com/zh/algorithm/graph/TopologicalSorting.java
@@ -30,9 +30,17 @@ public class TopologicalSorting {
             }
         }
 
-        /*for (Graph.Vertex value : vertices) {
-            System.out.println(value.name + "----" + value.inDegree);
-        }*/
+        DFS(vertices);
+
+    }
+
+    /*
+            Kahn算法拓扑排序: BFS(正向思维)
+     */
+    private static void BFS(Collection<Graph.Vertex> vertices) {
+    /*for (Graph.Vertex value : vertices) {
+        System.out.println(value.name + "----" + value.inDegree);
+    }*/
         // 3: 遍历图的每个节点：BFS（不要用普通的BFS，只在队列遍历的时候判断度数为0，只有当度数为0时才允许他加入到队列中）
         LinkedList<Graph.Vertex> queue = new LinkedList<>();    // 存储度数为0的节点
         // 给队列初始化
@@ -57,10 +65,44 @@ public class TopologicalSorting {
         // 判断是否出现环
         for (Graph.Vertex vertex : vertices) {
             if (vertex.inDegree != 0) {
-                throw new IllegalArgumentException("出现了环");
+                throw new IllegalArgumentException("BFS:出现了环");
             }
         }
     }
+
+    /*
+            遍历到最后以后回来的时候依次入栈即可（逆向思维）
+     */
+    public static void DFS(Collection<Graph.Vertex> vertices) {
+        LinkedList<Graph.Vertex> stack = new LinkedList<>();    // 存储访问元素的节点
+        // 对每个节点进行访问
+        for (Graph.Vertex vertex : vertices) {
+                DFSUtil(stack, vertex);
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.poll().name);
+        }
+    }
+
+    // 对vertex节点进行处理，根据递归特性，第一个压入的是图的最后节点，实现了拓扑排序
+    private static void DFSUtil(LinkedList<Graph.Vertex> stack, Graph.Vertex vertex) {
+        if (vertex.status == 1) {
+            throw new RuntimeException("DFS：发现环");
+        }
+        if (vertex.status == 2) {
+            return;
+        }
+        vertex.status = 1;
+        // 对该节点开始处理
+        for (Graph.Edge edge : vertex.edges) {
+            // 不能在这里判断是否被访问，就算被访问过了也需要进行递归以检测环
+            DFSUtil(stack, edge.linked);
+        }
+        vertex.status = 2;
+        // 节点处理完毕，压入栈中
+        stack.push(vertex);
+    }
+
 
     public static void main(String[] args) {
         // 1：构建图
@@ -80,7 +122,7 @@ public class TopologicalSorting {
         graph.addEdge("JavaWeb", "Spring框架");
         graph.addEdge("Spring框架", "微服务框架");
         graph.addEdge("微服务框架", "实战项目");
-        // graph.addEdge("实战项目", "微服务框架");  // 创建环的代码
+        graph.addEdge("实战项目", "微服务框架");  // 创建环的代码
 
         TS(graph);
 
