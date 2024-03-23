2a898dae8208647ee9131bc344a9822aa47596bd
AlbertZhang <han892577@163.com>
2024-03-23 21:54:29 +0800

    旅行商问题——DP

---
+++ src/com/zh/algorithm/dp/TSP.java
@@ -0,0 +1,15 @@
+package com.zh.algorithm.dp;
+
+/**
+ * @program: data-structures-and-algorithms
+ * @author: AlbertZhang
+ * @create: 2024-03-13 15:07
+ * @description: 旅行商问题——DP
+ **/
+public class TSP {
+    /*
+            旅行商问题是一个NP难问题，跟最小生成树容易混淆，最小生成树不用指定一个点并且返回
+        可以使用DP或者回溯解决，在DP中，使用Held-karp算法。
+
+     */
+}
