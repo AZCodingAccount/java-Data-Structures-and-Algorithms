4c5ce1bdf63cffaaedb4c6d33bf18f760f812588
AlbertZhang <han892577@163.com>
2024-04-24 21:50:51 +0800

    移动零——lc283

---
+++ src/com/zh/job/linkedlist/ReverseKGroup.java
@@ -0,0 +1,31 @@
+package com.zh.job.linkedlist;
+
+/**
+ * @program: data-structures-and-algorithms
+ * @author: AlbertZhang
+ * @create: 2024-04-16 18:26
+ * @description: K个一组翻转链表—lc25
+ **/
+public class ReverseKGroup {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+    }
+}
---
+++ src/com/zh/problem/doublep/MoveZeroes.java
@@ -0,0 +1,11 @@
+package com.zh.problem.doublep;
+
+/**
+ * @program: data-structures-and-algorithms
+ * @author: AlbertZhang
+ * @create: 2024-04-24 21:49
+ * @description: 移动零——lc283
+ **/
+public class MoveZeroes {
+
+}
