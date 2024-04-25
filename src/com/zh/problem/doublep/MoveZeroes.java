4fa9008d1ef9bc6987bbba6933b7bc5e47e91fa5
AlbertZhang <han892577@163.com>
2024-04-25 22:11:42 +0800

    移动零——lc283

--- src/com/zh/problem/doublep/MoveZeroes.java
+++ src/com/zh/problem/doublep/MoveZeroes.java
@@ -1,5 +1,7 @@
 package com.zh.problem.doublep;
 
+import java.util.Arrays;
+
 /**
  * @program: data-structures-and-algorithms
  * @author: AlbertZhang
@@ -7,5 +9,30 @@ package com.zh.problem.doublep;
  * @description: 移动零——lc283
  **/
 public class MoveZeroes {
+    /*
+            本题的核心逻辑是把非0元素全部移动到前面且保持相对顺序即可（不要关心0）    [1,1,0,3,12]
+            快慢指针i和j，慢指针i左边都是处理好的元素，快指针j去找不是0的元素进行交换。j>=nums.length结束
+     */
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 0;
+        while (j < nums.length - 1) {
+            // 移动i和j指针
+            while (nums[i] != 0 && j < nums.length - 2) {
+                i++;
+                j++;
+            }
+            j++;
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+        new MoveZeroes().moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
 }
