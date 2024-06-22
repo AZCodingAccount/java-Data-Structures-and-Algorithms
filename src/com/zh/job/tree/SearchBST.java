d9f716c042ce1480ee997a0e36db0e7728c32d5c
AlbertZhang <han892577@163.com>
2024-06-22 21:46:49 +0800

    二叉搜索树—挑战智商

---
+++ src/com/zh/job/tree/SearchBST.java
@@ -0,0 +1,27 @@
+package com.zh.job.tree;
+
+import com.zh.datastructures.tree.binarytree.TreeNode;
+
+/**
+ * @program: data-structures-and-algorithms
+ * @author: AlbertZhang
+ * @create: 2024-06-22 19:31
+ * @description: 二叉搜索树的搜索—lc700
+ **/
+public class SearchBST {
+    /*
+        这道题做不出来真可以回家种地了
+     */
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
