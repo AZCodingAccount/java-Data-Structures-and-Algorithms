543e56634f3767034dce4d0a9046b06f0734d667
AlbertZhang <han892577@163.com>
2024-07-28 22:03:52 +0800

    用一个队列实现栈

--- src/com/zh/job/stack/MyStack.java
+++ src/com/zh/job/stack/MyStack.java
@@ -5,79 +5,33 @@ import java.util.LinkedList;
 /**
  * @program: data-structures-and-algorithms
  * @author: AlbertZhang
- * @create: 2024-06-16 21:04
- * @description: 用队列实现栈—lc225
+ * @create: 2024-07-28 16:58
+ * @description: 一个队列实现栈—lc225
  **/
 public class MyStack {
-
-    /**
-     * 实际上我的实现很蠢，很不优雅也有很多冗余代码，可以使用一个队列实现，根据size控制（使用两队列真的横竖不得劲！！！）
-     * 实际上还有直接入栈的时候就控制队头是栈顶元素的，怎么实现都可以，就我这个实现有点抽象~~~
-     **/
-
-
-    private LinkedList<Integer> queue1 = new LinkedList<>();
-    private LinkedList<Integer> queue2 = new LinkedList<>();
+    LinkedList<Integer> queue = new LinkedList<>();
 
     public MyStack() {
+
     }
 
     public void push(int x) {
-        queue1.add(x);
+        int size = queue.size();
+        queue.offer(x);
+        for (int i = 0; i < size; i++) {
+            queue.offer(queue.poll());
+        }
     }
 
-    /**
-     * 弹出元素，这里需要将队列中的元素转移
-     **/
     public int pop() {
-        if (queue1.isEmpty()) {
-            while (queue2.size() > 1) {
-                queue1.add(queue2.poll());
-            }
-            return queue2.poll();
-        } else {
-            while (queue1.size() > 1) {
-                queue2.add(queue1.poll());
-            }
-            return queue1.poll();
-        }
+        return queue.poll();
     }
 
-    /**
-     * 查看栈顶元素，这里不需要删除，只需要查看最后一个元素
-     **/
     public int top() {
-        int res;
-        if (queue1.isEmpty()) {
-            while (queue2.size() > 1) {
-                queue1.add(queue2.poll());
-            }
-            res = queue2.peek();
-            queue1.add(queue2.poll());  // 这里一定要再加回去，不然顺序会乱。
-            // queue2  1      1  2                            1
-            // queue1  2   （2 1）        不加情况，会输出这个2   2
-            // 加回去变成，不加下一次遍历变成了2
-        } else {
-            while (queue1.size() > 1) {
-                queue2.add(queue1.poll());
-            }
-            res = queue1.peek();
-            queue2.add(queue1.poll());
-        }
-        return res;
+        return queue.peek();
     }
 
     public boolean empty() {
-        return queue1.isEmpty() && queue2.isEmpty();
-    }
-
-
-    public static void main(String[] args) {
-        MyStack myStack = new MyStack();
-        myStack.push(1);
-        myStack.push(2);
-        myStack.top();
-        myStack.pop();
-        myStack.pop();
+        return queue.isEmpty();
     }
 }
