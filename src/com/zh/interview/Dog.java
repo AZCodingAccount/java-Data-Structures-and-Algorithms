444fc505b43e32fe32c032eee1587e48a127ae17
AlbertZhang <han892577@163.com>
2024-08-03 20:47:48 +0800

    基础语法测试—子类

---
+++ src/com/zh/interview/Dog.java
@@ -0,0 +1,13 @@
+package com.zh.interview;
+
+/**
+ * @program: data-structures-and-algorithms
+ * @author: AlbertZhang
+ * @create: 2024-07-30 21:40
+ * @description:
+ **/
+public class Dog extends Animal{
+    public static void main(String[] args) {
+
+    }
+}
