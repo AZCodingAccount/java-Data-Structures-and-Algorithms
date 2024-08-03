f5d7583e77c15b70aafc1a0f90200c79dbf44cba
AlbertZhang <han892577@163.com>
2024-08-03 20:47:36 +0800

    基础语法测试—父类

---
+++ src/com/zh/interview/Animal.java
@@ -0,0 +1,17 @@
+package com.zh.interview;
+
+import java.util.Arrays;
+
+/**
+ * @program: data-structures-and-algorithms
+ * @author: AlbertZhang
+ * @create: 2024-07-30 21:40
+ * @description: 动物
+ **/
+public abstract class Animal {
+     int a;
+
+    public static void main(String[] args) {
+        int[] b=new int[10];
+    }
+}
